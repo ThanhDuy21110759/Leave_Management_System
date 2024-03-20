c9a6a2481955a1f5f9123d43dde710ec10a3d97e
ThanhDuy21110759 <21110759@student.hcmute.edu.vn>
2024-03-21 00:21:02 +0700

    commit 21/03/2024 12:20 PM

--- src/main/java/com/example/management/controller/AuthController.java
+++ src/main/java/com/example/management/controller/AuthController.java
@@ -57,7 +57,7 @@ public class AuthController {
                 .map(item -> item.getAuthority())
                 .collect(Collectors.toList());
 
-        return ResponseEntity.ok(new JwtResponse(jwt));
+        return ResponseEntity.ok(new JwtResponse(jwt,roles));
     }
 
     @PostMapping("/signup")
--- src/main/java/com/example/management/payload/response/JwtResponse.java
+++ src/main/java/com/example/management/payload/response/JwtResponse.java
@@ -1,27 +1,37 @@
 package com.example.management.payload.response;
 
+import com.example.management.entity.Role;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class JwtResponse {
     private String token;
     private String type = "Bearer";
-    public JwtResponse(String token) {
+    private List<String> roles = new ArrayList<>();
+
+    public JwtResponse(String token, List<String> roles) {
         this.token = token;
+        this.roles = roles;
     }
 
     public String getAccessToken() {
       return token;
     }
-
     public void setAccessToken(String accessToken) {
       this.token = accessToken;
     }
-
     public String getTokenType() {
       return type;
     }
-
     public void setTokenType(String tokenType) {
       this.type = tokenType;
+    }
+    public List<String> getRoles() {
+        return roles;
+    }
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
     }
 }
